1c835021429d8c2e9e26a977ba3ee27b49ccc6c5
Joseph Reilly <reilly4181@gmail.com>
2019-03-29 23:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 var recipes = {};
 
 
-// function updateObjectWithKeyAndValue(object, key, value){
-//   return Object.assign({}, object, {[key]: value})
-// }
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
 
 
-// function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-//   object[key] = value
-//   return object
-// }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
 
-// var object = {prop:1}
+var object = {prop:1}
 
-// destructivelyUpdateObjectWithKeyAndValue(object, 'prop2', 2)
-// object
-
-
-// function deleteFromObjectByKey(object, key){
-//   delete object.key
-//   return object
-// }
-
-// var object = {prop:1}
-
-// var newobject = deleteFromObjectByKey(object, 'prop')
-// object
+destructivelyUpdateObjectWithKeyAndValue(object, 'prop2', 2)
+object
 
 
+function deleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
 
-// function deleteFromObjectByKey(object, key){
-//   var newObject = Object.assign({}, object)
-//   delete newObject[key]
-//   return newObject
-// }
+var object = {prop:1}
+
+var newobject = deleteFromObjectByKey(object, 'prop')
+object
 
 
 
-
-// function destructivelyDeleteFromObjectByKey(object, key){
-//   var newObject = delete object.key
-//   return newObject
-// }
+function deleteFromObjectByKey(object, key){
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
 
 
 
 
-// function destructivelyDeleteFromObjectByKey(object, key){
-//   delete object[key]
-//   return object
-// }
+function destructivelyDeleteFromObjectByKey(object, key){
+  var newObject = delete object.key
+  return newObject
+}
+
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
